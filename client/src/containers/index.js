b31127027ad06a9b7687b233e4db2cafebf9fd79
sleeplesseditor <josh@bryson.me.uk>
2018-09-23 02:08:31 +0100

    Container created

---
+++ client/src/containers/index.js
@@ -0,0 +1,8 @@
+import AddGameContainer from './AddGameContainer';
+import GamesContainer from './GamesContainer';
+
+// We export all the containers at once
+export {
+  AddGameContainer,
+  GamesContainer
+};
--- client/src/routes.js
+++ client/src/routes.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import { Router, Route, hashHistory, IndexRoute } from 'react-router';
-import { Home, Welcome, About, Contact } from './components';
+import { AddGameContainer, GamesContainer } from './containers';
+import { Home, Archive, Welcome, About, Contact } from './components';
 
-// Use hashHistory for easier development
 const routes = (
   <Router history={hashHistory}>
     <Route path="/" component={Home}>
       <IndexRoute component={Welcome} />
       <Route path="/about" component={About} />
       <Route path="/contact" component={Contact} />
+    </Route>
+    <Route path="/games" component={Archive}>
+      <IndexRoute component={GamesContainer} />
+      <Route path="add" component={AddGameContainer} />
     </Route>
   </Router>
 );
