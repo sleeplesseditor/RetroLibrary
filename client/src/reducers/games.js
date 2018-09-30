452217d220827885a10506c38621ee90bf3eb088
sleeplesseditor <josh@bryson.me.uk>
2018-09-30 11:41:06 +0100

    Further Redux setup

--- client/src/reducers/games.js
+++ client/src/reducers/games.js
@@ -6,14 +6,11 @@ import {
 
 const initialState = Immutable.Map();
 
-// That's a very standard reducer function to return a new state given a dispatched action
 export default (state = initialState, action) => {
   switch (action.type) {
-  // GET_GAMES_SUCCESS case return a new state with the fetched games in the state
     case GET_GAMES_SUCCESS: {
       return state.merge({ list: action.games });
     }
-  // In case of failure it simplies returned a new empty state
     case GET_GAMES_FAILURE: {
       return state.clear();
     }
